36f76600cdd1395b1630e536b91af75307a19ea0
Rohit Sharma <rohit.sharma1048@gmail.com>
2023-08-28 07:18:49 -0500

    Add Checkout Service Request and Response DTOs

---
+++ CheckoutService/Dtos/CheckoutRequestDto.cs
@@ -0,0 +1,8 @@
+namespace ECommerceApp.CheckoutService.Dtos
+{
+    /// <summary> DTO for checkout request. </summary>
+    public class CheckoutRequestDto
+    {
+        public List<string> WatchIds { get; set; }
+    }
+}
---
+++ CheckoutService/Dtos/CheckoutResponseDto.cs
@@ -0,0 +1,8 @@
+namespace ECommerceApp.CheckoutService.Dtos
+{
+    /// <summary> DTO for checkout response. </summary>
+    public class CheckoutResponseDto
+    {
+        public decimal Price { get; set; }
+    }
+}
